6653f0f14e613534e9c05eb98bab2a5eb0ef8878
Pavel Svec <pavel.svec@itcrawl.cz>
2020-12-11 10:55:19 +0100

    Rozbor HttpResponseMessage

--- WorkWithHttpClient/Program.cs
+++ WorkWithHttpClient/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace WorkWithHttpClient
 {
@@ -48,16 +49,43 @@ namespace WorkWithHttpClient
             var earthJSON = JsonConvert.SerializeObject(earth);
 
             HttpContent content = new StringContent(earthJSON, Encoding.UTF8, "application/json");
+            // StringContent dědí z ByteContent a ten dědí z abstraktní HttpContent
 
             HttpClient htc = new HttpClient();
 
             try
             {
                 // tohle je testovací sink pro post/get requesty
-                var response = await htc.PostAsync("http://ptsv2.com/t/ssps/post", content);
+                HttpResponseMessage response = await htc.PostAsync("http://ptsv2.com/t/ssps/post", content);
+
+                Console.WriteLine("Headers");
+                foreach (KeyValuePair<string, IEnumerable<string>> v in response.Headers )
+                {
+                    Console.Write($"Klíč {v.Key}, Hodnota: ");
+                    foreach (var t in v.Value)
+                    {
+                        Console.Write($"{t}");
+                    }
+                    Console.WriteLine();
+                }
+
                 Console.WriteLine(await response.Content.ReadAsStringAsync());
                 Console.WriteLine();
-                Console.WriteLine($"Úspěch: {response.IsSuccessStatusCode} Status: {response.StatusCode}");
+
+                Console.WriteLine("TrailingHeaders");
+                foreach (KeyValuePair<string, IEnumerable<string>> v in response.TrailingHeaders)
+                {
+                    Console.Write($"Klíč {v.Key}, Hodnota: ");
+                    foreach (var t in v.Value)
+                    {
+                        Console.Write($"{t}");
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine($"\nÚspěch: {response.IsSuccessStatusCode} Status: {response.StatusCode}");
+
+                Console.WriteLine(response);
             }
             catch (HttpRequestException hre)
             {
