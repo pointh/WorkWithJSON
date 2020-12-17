44deb570b64e28316aee66c5b8b5f73da362af53
Pavel Svec <pavel.svec@itcrawl.cz>
2020-12-17 12:40:20 +0100

    Přidané komentáře a List<SpaceBody>

--- WorkWithHttpClient/WorkWithHttpClient.cs
+++ WorkWithHttpClient/WorkWithHttpClient.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 
 namespace WorkWithHttpClient
 {
+ 
     public class SpaceBody
     {
         [JsonProperty("nameOfBody")]
@@ -21,9 +22,12 @@ namespace WorkWithHttpClient
         [JsonProperty("moon")]
         public Moon m { get; set; }
 
+        [JsonIgnore]
+        public bool IsInhabited { get; set; }
+
         public override string ToString()
         {
-            return $"{Name}:{Mass}:{Distance}:{m?.MoonName}";
+            return $"{Name}:{Mass}:{Distance}:{m?.MoonName}:{IsInhabited}";
         }
     }
 
@@ -34,8 +38,9 @@ namespace WorkWithHttpClient
     }
 
 
-    class Program
+    class WorkWithHttpClient
     {
+        // async ještě probereme, teď ho berte tak jak je.
         static async Task Main(string[] args)
         {
             SpaceBody earth = new SpaceBody
@@ -43,20 +48,33 @@ namespace WorkWithHttpClient
                 Name = "Země",
                 Mass = 1.0,
                 Distance = 1.0,
-                m = new Moon { MoonName = "Měsíc" }
+                m = new Moon { MoonName = "Měsíc" },
+                IsInhabited = false
             };
 
             var earthJSON = JsonConvert.SerializeObject(earth);
 
+            
             HttpContent content = new StringContent(earthJSON, Encoding.UTF8, "application/json");
             // StringContent dědí z ByteContent a ten dědí z abstraktní HttpContent
 
+            // Toto je klient, který funguje stejně jako náš browser, jen je s ním těžší pořízení
             HttpClient htc = new HttpClient();
 
             try
             {
                 // tohle je testovací sink pro post/get requesty
-                HttpResponseMessage response = await htc.PostAsync("http://ptsv2.com/t/ssps/post", content);
+                // Sink ssps jsem vytvořil před týdnem, pokud ho smazali, vytvořte si "New Random Toilet"
+                // a nově vytvořenou adresu vložte do následujícího odkazu.
+                // Na adrese, ve které vynecháte /post (http://ptsv2.com/t/cqzh9-1608204865) uvidíte obsah toho, 
+                // co jsete poslali na server
+
+                // HttpResponseMessage není tak jednoduchá, jak by se mohlo zdát
+                // breakpoint za voláním PostAsync ukáže, co všechno se přenáší mezi klientem a serverem ...
+
+                // await pošle úlohu do thread pool vlákna. V něm zavolá metodu PostAsync a pošle content
+                // na server na uvedené adrese
+                HttpResponseMessage response = await htc.PostAsync("http://ptsv2.com/t/cqzh9-1608204865/post", content);
 
                 Console.WriteLine("Headers");
                 foreach (KeyValuePair<string, IEnumerable<string>> v in response.Headers )
@@ -69,6 +87,8 @@ namespace WorkWithHttpClient
                     Console.WriteLine();
                 }
 
+                // Content může být rozsáhlý, proto pokud nechceme blokovat vlákno uživatelského rozhraní,
+                // provedeme načtení na thread pool
                 Console.WriteLine(await response.Content.ReadAsStringAsync());
                 Console.WriteLine();
 
@@ -87,6 +107,7 @@ namespace WorkWithHttpClient
 
                 Console.WriteLine(response);
             }
+            // když něco selže ...
             catch (HttpRequestException hre)
             {
                 Console.WriteLine("Chyba: Server neslyší:" + hre);
---
+++ WorkWithJSON/WorkWithJSON.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+// Newtonsoft.Json (https://www.newtonsoft.com/json) je nejpoužívanější knihovnou pro převod třídy do JSON a naopak
+// Není součástí .NET, ale je volně dostupná jako rozšiřující balíček Nuget
+// Instalace: viz video na virtuální škole a v Teamsech
+// 
+// Používá atributy u vlastností. Atributy popisují, jak bude vypadat reprezentace vlastnosti
+// v JSON - přídávají k vlastnosti identifikátor(jméno v JSON).
+
+// Pokud nepřidáme atributy k vlastnostem, budou se v JSON objevovat s identifikátorem, který je stejný jako jméno
+// vlastnosti.
+using Newtonsoft.Json;
+
+namespace WorkWithJSON
+{
+
+    public class SpaceBody
+    {
+        [JsonProperty("nameOfBody")]
+        public string Name { get; set; }
+
+        [JsonProperty("massOfBody")]
+        public double Mass { get; set; } // * mass of Earth = mass in kg
+
+        [JsonProperty("distanceFromSun")]
+        public double Distance { get; set; } // AU = 1.495978707 . 10exp(11) m
+
+        [JsonProperty("moon")]
+        public Moon m { get; set; }
+
+        [JsonIgnore]
+        public bool IsInhabited { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}:{Mass}:{Distance}:{m?.MoonName}:{IsInhabited}";
+        }
+    }
+
+    public class Moon
+    {
+        [JsonProperty("moonName")]
+        public string MoonName { get; set; }
+    }
+
+ /* Takto bude vypadat převod z objektu typu SpaceBody na JSON:
+  * 
+  * {"nameOfBody":"Země","massOfBody":1.0,"distanceFromSun":1.0,"moon":{"moonName":"Měsíc"}}
+  * 
+  * 1. IsInhabited se nebude převádět, protože má atribut JsonIgnore
+  * 2. Argument v JsonProperty se použije pro pojmenování JSON elementu
+  * 3. Vnořené objekty v C# mají svou obdobu ve vnořených objektech v JSON:
+  *    SpaceBody{Moon} odpovídá v JSON {"moon":{"moonName":"Měsíc"}}
+  *    
+  *  Převod opačným směrem používá názvy elementů pro inicializaci objektu a provádí datové
+  *  konverze podle typu vlastností.
+  */
+
+    class WorkWithJSON
+    {
+        static void Main(string[] args)
+        {
+            SpaceBody earth = new SpaceBody
+            {
+                Name = "Země",
+                Mass = 1.0,
+                Distance = 1.0, 
+                m = new Moon { MoonName = "Měsíc"}
+            };
+
+            SpaceBody mars = new SpaceBody
+            {
+                Name = "Mars",
+                Mass = 0.107,
+                Distance = 1.3815
+            };
+
+            var earthJSON = JsonConvert.SerializeObject(earth);
+
+            var druhaZeme = JsonConvert.DeserializeObject<SpaceBody>(earthJSON);
+
+            var marsJSON = JsonConvert.SerializeObject(mars);
+
+            var druhyMars = JsonConvert.DeserializeObject<SpaceBody>(marsJSON);
+
+            // Merkur nemá měsíc
+            string mercuryJSON = "{\"nameOfBody\":\"Merkur\",\"massOfBody\":0.056,\"distanceFromSun\":0.4}";
+            var mercury = JsonConvert.DeserializeObject<SpaceBody>(mercuryJSON);
+            /*
+            * Je stejné jako SpaceBody mercury = new SpaceBody
+            * {
+            *      Name = "Merkur",
+            *      Mass = 0.056,
+            *      Distance = 0.4,
+            *      m = null,
+            *      IsInhabited = false
+            *  }
+            *  
+            *  Při převodu z JSON do objektu musíme v generické metodě uvést, do jakého typu chceme JSON převádět (<SpaceBody>)
+            */
+
+            List<SpaceBody> planets = new List<SpaceBody> { earth, mars, druhaZeme, druhyMars, mercury };
+
+            foreach (var x in planets)
+                Console.WriteLine(x);
+
+            var planetsJSON = JsonConvert.SerializeObject(planets);
+
+            // Na výstupu je vidět, jak se v JSON realizuje List ...
+            Console.WriteLine("\n" + planetsJSON + "\n");
+
+            // A zcela intuitivně funguje i konverze směrem zpět
+            // V tomto případě vytváříme objekt typu List<SpaceBody>, který se musí
+            // objevit ve volání DeserializeObject
+            List<SpaceBody> anotherPlanets = JsonConvert.DeserializeObject<List<SpaceBody>>(planetsJSON);
+
+            foreach (var x in anotherPlanets)
+                Console.WriteLine(x);
+        }
+    }
+}
