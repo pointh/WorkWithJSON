a27b2bea76ff43e072926a908f0388217644cc5f
Pavel Svec <pavel.svec@itcrawl.cz>
2022-09-30 15:56:31 +0200

    Init

---
+++ IndexGenerator/IndexGenerator/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace IndexGenerator
+{
+    public enum IndexError { OK, NoSpace, TooLong }
+    class IndexGenerator
+    {
+        private Dictionary<string, List<int>> index;
+        private StringBuilder sb; 
+
+        public IndexGenerator()
+        {
+            index = new Dictionary<string, List<int>>();
+            sb = new StringBuilder(255);
+        }
+
+        public IndexError AddToIndex(string s)
+        {
+            int i = 0;
+            StringBuilder sb = new StringBuilder(255);
+
+            while(i < s.Length)
+            {
+                if (char.IsLetter(s[i]))
+                {
+                    sb.Append(s[i]);
+                }
+                else
+                {
+                    if(sb.Length > 1)
+                    {
+                        if (index.ContainsKey(sb.ToString()) == false)
+                        {
+                            index.Add(sb.ToString(), new List<int>(new int[] { i-sb.Length }));
+                        }
+                        else
+                        {
+                            index[sb.ToString()].Add(i-sb.Length);
+                        }
+                    }
+                    sb.Clear();
+                }
+                i++;
+            }
+            return IndexError.OK;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            foreach(KeyValuePair<string, List<int>> kv in index)
+            {
+                sb.Append(kv.Key + '\t');
+                sb.AppendLine(string.Join(",", kv.Value));
+            }
+            return sb.ToString();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt = File.ReadAllText("Text.txt");
+            IndexGenerator ig = new IndexGenerator();
+
+            Stopwatch sw = Stopwatch.StartNew();
+            ig.AddToIndex(txt);
+            sw.Stop();
+
+            Console.WriteLine(ig);
+            Console.WriteLine($"Čas zpracování: {sw.ElapsedMilliseconds} ms.");
+        }
+    }
+}
